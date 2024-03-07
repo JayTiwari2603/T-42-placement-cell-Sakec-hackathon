2deda22c10f314106820a3af08d310e8b665eb24
Jayantkumar Tiwari <158484490+JayTiwari2603@users.noreply.github.com>
2024-03-07 12:13:38 +0530

    files uploads

---
+++ vega/public/javascripts/Resource.js
@@ -0,0 +1,21 @@
+const express=require('express')
+
+
+
+// script.js
+document.getElementById("sem1").addEventListener("click", function() {
+    // Redirect to the Semester 1 notes page (replace with the actual URL)
+    
+});
+
+document.getElementById("sem2").addEventListener("click", function() {
+    // Redirect to the Semester 2 notes page (replace with the actual URL)
+    window.location.href = "sem2.html";
+});
+
+
+
+
+
+
+
